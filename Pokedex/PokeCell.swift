905539ef07429e6424ceaed3de6485e7c0fc405f
dpelovski@icloud.com <dpelovski@icloud.com>
2017-03-20 20:45:28 +0200

    Created the most part of the visual interface. Uploaded some images that were missing in the first place. Added custom PokeCell for managing the collection view cells.

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,36 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Damian Pelovski on 3/20/17.
+//  Copyright © 2017 Damian Pelovski. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: String(self.pokemon.pokedexId))
+        
+    }
+    
+    
+    
+}
